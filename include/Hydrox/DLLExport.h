eca52dc542298fd4841220b34e8df339803c6b6c
Christoph Laemmerhirt <gippi.laemmi@t-online.de>
2013-12-13 21:42:24 +0100

    Made the library compilation of the header files in the engine platform independent.

--- include/Hydrox/DLLExport.h
+++ include/Hydrox/DLLExport.h
@@ -1,7 +1,17 @@
-#ifdef DLL_EXPORT
-#define GRAPHICAPI __declspec(dllexport)
-#define EXPIMP_TEMPLATE
+#if defined(_WIN32) || defined(__WIN32__)
+  #ifdef DLL_EXPORT
+    #define GRAPHICAPI __declspec(dllexport)
+    #define EXPIMP_TEMPLATE
+  #else
+    #define GRAPHICAPI __declspec(dllimport)
+    #define EXPIMP_TEMPLATE extern
+  #endif
 #else
-#define GRAPHICAPI __declspec(dllimport)
-#define EXPIMP_TEMPLATE extern
+  #if __GNUC__ >= 4
+    #define GRAPHICAPI __attribute__ ((__visibility__ ("default")))
+    #define EXPIMP_TEMPLATE
+  #else
+    #define GRAPHICAPI __attribute__ ((__visibility__ ("default")))
+    #define EXPIMP_TEMPLATE extern
+  #endif
 #endif
